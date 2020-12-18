803779c72c659fa215466b13873833923747de72
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-12-18 18:36:20 +1000

    feat: Добавь БД на монго

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/newsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
